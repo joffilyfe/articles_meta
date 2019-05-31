f87b47011458c3225e5171c40c42bff62ad8758c
Joffily Ferreira <joffily.ferreira@scielo.org>
2019-05-31 12:03:16 -0300

    Adiciona `item_number_type` ao `publisher_item`
    Este commit resolve a issue #167 onde o elocation-id é referênciado pelo campo item_number_type.

--- articlemeta/export.py
+++ articlemeta/export.py
@@ -206,6 +206,7 @@ class Export(object):
             export_crossref.XMLArticlePubDatePipe(),
             export_crossref.XMLPagesPipe(),
             export_crossref.XMLPIDPipe(),
+            export_crossref.XMLElocationPipe(),
             export_crossref.XMLDOIDataPipe(),
             export_crossref.XMLDOIPipe(),
             export_crossref.XMLResourcePipe(),
